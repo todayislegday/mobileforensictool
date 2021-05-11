f8800deaa6bbadd85a42b98ebc4320b86d579db9
TimyLoom <dldydduq456@naver.com>
2021-05-11 12:53:35 +0900

    .

--- function/connectdb.py
+++ function/connectdb.py
@@ -4,17 +4,22 @@
 import sqlite3
 
 
-def connectdb(excutes,dbname,table,field):
+def connectdb(dbname,table,field,excutes=None):
     f = open("../경로.txt", 'r')
     path = f.readlines()[1]
     f.close()
-
+    path+='/data/com.samsung.android.providers.contacts/databases/' #추후 파일경로 list에서 읽어옴
     conn = sqlite3.connect(f"{path}/{dbname}")#해당 경로의 db를 sqlite3로 연결
     cur = conn.cursor()
-    cur.execute("select {field} from {table}")#sql 문,명령문은 
+    cur.execute(f"select * from {table}")#sql 문,명령문은 
     rows = cur.fetchall() #가져온 결과의 행을 리스트로 가져옴
-    for row in rows: 
-        print(row) 
+    # for row in rows: 
+    #     print(row) 
+    cur.close()
     conn.close()
+    return rows
+    
+
+#print(connectdb('calllog.db','calls','*'))
 
 
