0e37cdab408350928947faf252dca3d03753ad3f
whfflssns <wlghdlekt20@gmail.com>
2021-05-20 14:30:15 +0900

    read_tar_add_image

--- function/read_tar.py
+++ function/read_tar.py
@@ -1,18 +1,23 @@
 import tarfile
+import os
 ### tar파일에서 원하는 폴더 내용 긁어오기.
 ### file_path는 tar경로, output_path는 외부로 db파일
 
 target_folder = set(['./data/com.samsung.android.providers.contacts/databases/', './data/com.samsung.cmh/databases/', './data/com.samsung.android.messaging/databases/',
                       './user_de/0/com.android.providers.telephony/databases/', './data/com.android.chrome/', './data/com.sec.android.app.sbrowser/',
                      './data/com.android.vending/databases/', './data/com.android.providers.downloads/databases/', './data/com.samsung.android.providers.media/',
-                      './data/com.android.providers.media/databases/'])
-
-
+                      './data/com.android.providers.media/databases/', './media/'])
+image_folder = set(['.png', '.jpg', '.jpeg', '.gif', '.mp4'])
 
 def decompression(file_path, output_path):
 
     tar = tarfile.open(file_path)
     for folder in target_folder:
+        if folder == './media/':
+            for img in image_folder:
+                image_name = set(tarinfo for tarinfo in tar.getmembers() if (tarinfo.name.startswith('./media/') and tarinfo.name.endswith(img)))
+                tar.extractall(members=image_name, path=os.path.dirname(os.path.abspath('./was/static/assets/images/widget')))
+            continue
         target_name = set(tarinfo for tarinfo in tar.getmembers() if tarinfo.name.startswith(folder))  # target_folder에 있는 폴더의 이름을 가진 모든 tarinfo를 target_name으로 둔다.
         tar.extractall(members=target_name, path=output_path)
 
