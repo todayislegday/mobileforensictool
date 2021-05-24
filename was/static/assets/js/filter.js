a578e975d12a944e707056d2abe16f5b2a333bac
TimyLoom <dldydduq456@naver.com>
2021-05-24 15:53:28 +0900

    update filter

--- was/static/assets/js/filter.js
+++ was/static/assets/js/filter.js
@@ -7,7 +7,6 @@ function filter(){
     for(i=0; i<list.length; i++) {
         name = list[i].children;
         for(j=0; j<name.length; j++) {
-            console.log(name[j])
             if(name[j].innerHTML.toUpperCase().indexOf(keyword)>-1) {
                list[i].style.display="";
                break;
@@ -17,3 +16,16 @@ function filter(){
         }
     }
 }
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $("#keyword").keyup(function(e) {
+        if($(this).val()==''){
+console.log(1)
+console.log( $("#buttonset").find('li[class="page-item active"]').children("button").attr("id") );
+     var id=$("#buttonset").find('li[class="page-item active"]').children("button").attr("id")
+     id=id.substring(2,3)
+    $(`.list_data[id!=${id}]`).css("display","none")
+}
+    });
+});
