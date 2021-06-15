9c4490b5ac5601abc67740f435d2a2c4d161049b
TimyLoom <dldydduq456@naver.com>
2021-06-15 09:46:43 +0900

    update

--- was/manage.py
+++ was/manage.py
@@ -3,9 +3,9 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
+    sys.argv=[os.path.abspath(__file__),'runserver']
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -15,6 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     
     execute_from_command_line(sys.argv)
 
